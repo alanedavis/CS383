dbd392efb7a111203508a7282a6666dd3fe51b90
alanedavis <alan.davis.el@gmail.com>
2019-07-26 16:21:24 -0400

    Desktop

--- HW2/aed84_hw2.m
+++ HW2/aed84_hw2.m
@@ -11,50 +11,44 @@ X = data(data(:,1) == 1,:);
 Y = data(data(:,1) == -1,:);
 
 %% Standardizing data
-% Simply standardizes the data in preparation of performing PCA on the
-% data. Also, removing the first column which is null.
-
-Xm = mean(X);
-Xs = std(X);
-
-X_std = X - repmat(Xm,size(X,1),1);
-X_std = X ./ repmat(Xs,size(X,1),1);
-
-Ym = mean(Y);
-Ys = std(Y);
-
-Y_std = Y - repmat(Ym,size(Y,1),1);
-Y_std = Y ./ repmat(Ys,size(Y,1),1);
-
-X_std(:,1) = [];
-Y_std(:,1) = [];
 
 %% Random k Value
 rng(0);
 k = randi(7);
        
 %% Run Function
-myKMeans(X_std,k,Y);
+myKMeans(X_std,k);
 
 %% Functions
-function myKMeans(X,k,Y)
-    % Imlibrary
-    imlibrary = [];
+function myKMeans(X,k)
+    % Simply standardizes the data in preparation of performing PCA on the
+    % data. Also, removing the first column which is null.
+    Xm = mean(X);
+    Xs = std(X);
 
+    X_std = X - repmat(Xm,size(X,1),1);
+    X_std = X ./ repmat(Xs,size(X,1),1);
+
+    Ym = mean(Y);
+    Ys = std(Y);
+
+    Y_std = Y - repmat(Ym,size(Y,1),1);
+    Y_std = Y ./ repmat(Ys,size(Y,1),1);
+
+    X_std(:,1) = [];
+    Y_std(:,1) = [];
+    
     % Rows and Cols
-    [rows, cols] = size(X);
+    [rows, cols] = size(X_std);
 
     % Colors
-    colors = [ 0.8500,0.3250,0.0980; 0.4940,0.1840,0.5560;
-           0.3010, 0.7450, 0.9330; 0.6350, 0.0780, 0.1840;
-           0.9290,0.6940,0.1250; 0,0.4470,0.7410;
-           0.4660, 0.6740, 0.1880; ];
+    colors = ['b','r','g','y','c','m'];
 
     % Dimensionality Reduction & Cluster Formation
-    if size(X,2) > 3
-        coeff = pca(X);
+    if size(X_std,2) > 3
+        coeff = pca(X_std);
         reduce = coeff(:,1:3);
-        X = X * reduce;
+        X_std = X_std * reduce;
     end
 
     % Hard Coding that k will only be 1-7
@@ -62,133 +56,63 @@ function myKMeans(X,k,Y)
         k = 7;
     end
 
-    % Finding min and max of X
-    means = mean(X);
-
     % Initialize reference vectors
-    kindex = randperm(rows,k);
+    kvecs = [];
+    for o = 1:k
+        row = X_std(randi([1,rows]),:);
+        kvecs = [kvecs; row;];
+    end
+    
+    % Means
+    index = randperm(rows,k); %Vector of k random numbers
     means = [];
-    for i = 1:length(kindex)
-        means = [means;X(kindex(i),:)];
+    for i = 1:length(index) %Vectorize starting means
+        means = [means;X_std(index(i),:)];
     end
-
-    % Distance Calculations D = [D(X1,mean1),D(X1,mean2),...D(X1,meank)]
-    % Vector distance from X(j,:) to mean 
-    DM = 1;
-    iter = 0;
-    while abs(DM) > (2^-23)
-    iter = iter+1;
-    D = [];
-    for i = 1:k
-        calc = [];
-        for j = 1:rows
-            calc = [calc;norm(means(i,:)-X(j,:))];
-        end
-        D = [D,calc];
-    end
-
-    %Cluster
-    %find index of min distance
-    %stick that observation in that cluster
-    C(1:rows,1:3,1:k) = 0;
-    for i = 1:rows
-        [v,index] = min(D(i,:));
-        C(i,:,index) = X(i,:);
-    end
-
-    %Calculate Purity
-    clusterPurity = 0;
-    Purity = 0;
-    for i = 1:k %For every Cluster
-        temp = C(:,:,i); % Copy cluster into temp
-        temp = [temp,X];
-        temp2 = [];
-        for j = 1:rows
-            if temp(j,1) == 0 && temp(j,2) == 0 && temp(j,3) == 0 %Remove 0 vectors
-                continue
-            else
-                temp2 = [temp2;temp(j,:)]; % Recombined X and Y
+    
+    % Making Clusters
+    iter = 1;
+    change = 1;
+    while abs(change) > 2^(-23)
+        iter = iter + 1;
+        D = [];
+        
+        for i = i:k
+            calc = [];
+            for j = 1:rows
+                calc = [calc; norm(means(i,:)-X_std(j,:))];
             end
+            D = [D,calc];
+
+        C(1:rows,1:3,1:k) = 0; %Create MultiDimensional Matrix
+        for i = 1:rows 
+            [v,index] = min(D(i,:)); %find index of min distance
+            C(i,:,index) = X_std(i,:); %stick that observation in that cluster 
         end
-        pos = sum(temp2(:,4)==1); %Count classes
-        neg = sum(temp2(:,4)==(-1));
-        Purity = Purity + max(pos,neg); %Purity Calculations
-    end
-    Purity = Purity/rows; %Composite Purity
-
-    %Plot Clusters
-    %Title 'Iteration n - Purity = ...'
-    %Clear Last Figure
-    %For every Cluster
-    %Select Color
-    %Plot Cluster i
-    %Plot Mean in Same Colo
-    tit = ['Iteration ',num2str(iter),' -',' Purity = ',num2str(Purity)];
-    clf
-    figure1 = figure(1);
-    for i = 1:k
-        color = colors(i,1:3);
-        scatter3(C(:,1,i),C(:,2,i),C(:,3,i),36,color,'x')
-        if i == 1
-            hold on
+        
+        cpurity = 0;
+        purity = 0;
+        
+        for i = 1:k
+            temp = C(:,:,i);
+            temp = [temp X(:,1)];
+            holder = [];
+            for j = 1:rows
+                if temp(j,1) == 0 && temp(j,2) == 0 && temp(j,3) == 0
+                    continue
+                else
+                    holder = [holder; temp(j,:)];
+                end
+            end
+            
+            one = sum(X(:,1) == 1);
+            negone = sum(X(:,1) == -1);
+            purity = purity + max(one,negone);
         end
-        scatter3(means(i,1),means(i,2),means(i,3),75,'MarkerEdgeColor','k','MarkerFaceColor',color);
-        title(tit);
-    end
+        purity = purity / rows;
 
-    %Save Plots 
-    %Create Plots Folder and Add to Work Directory
-    %Name the plots and add them to the library
-    if ~exist('plots', 'dir')
-       mkdir('plots')
-       addpath('plots')
-    end
-    imname = ['plots/figure',num2str(iter),'.jpg'];
-    imlibrary = [imlibrary;string(imname)];
-    saveas(figure1,imname) 
-
-    % pause(1) Pause for viewer in debugging
-    % Calculate new means
-    % Move cluster to temp working space
-    % Strip 0 vectors
-    % Compute mean
-    oldmeans = means;       
-    means = [];
-    for i = 1:k
-        temp = C(:,:,i);
-        temp = temp(any(temp,2),:);
-        means = [means;mean(temp)];
-    end
-
-    % Calculate Manhattan distance D(oldmean,mean)
-    DM = 0;
-    for i = 1:k
-        Di = (means(i,1)-oldmeans(i,1))+(means(i,2)-oldmeans(i,2))+(means(i,3)-oldmeans(i,3));
-        DM = DM + Di;
-    end
-
-    end
-
-    % Construct Video
-    % create the video object
-    % video.FileFormat = 'mp4';
-    % open the file for writing
-    % where N is the number of images
-    % read the next image
-    % write the image to file
-    % close the file
-    vname = ['K_',num2str(k),'_F_all'];
-    video = VideoWriter(vname);
-    video.FrameRate = 1;
-    open(video);
-    [numImg,c] = size(imlibrary);
-    for ii=1:numImg
-      I = imread(char(imlibrary(ii))); 
-      writeVideo(video,I); 
-    end
-    close(video);
-
-    if exist('plots', 'dir')
-       rmdir('plots','s')
+        end
+  
+       end
     end
 end
